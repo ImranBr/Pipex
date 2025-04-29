eccc9819e135188c43ae572fe0bc4db3a78d57e6
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-04-30 01:10:33 +0200

    ex pipe good

--- objs/ex_Pipe.d
+++
@@ -1,2 +0,0 @@
-objs/ex_Pipe.o: srcs/ex_Pipe.c includes/pipex.h \
-  lib/ft_printf/includes/ft_printf.h lib/libft/includes/libft.h
--- objs/ex_fork.d
+++
@@ -1,2 +0,0 @@
-objs/ex_fork.o: srcs/ex_fork.c includes/pipex.h \
-  lib/ft_printf/includes/ft_printf.h lib/libft/includes/libft.h
