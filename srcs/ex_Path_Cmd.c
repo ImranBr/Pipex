3324a124547cad23b7f49aec157f6ad8f82c7b8a
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-05-02 22:29:35 +0200

    get_cmd new

--- srcs/ex_Path_Cmd.c
+++ srcs/ex_Path_Cmd.c
@@ -6,7 +6,7 @@
 /*   By: ibarbouc <ibarbouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:36:54 by ibarbouc          #+#    #+#             */
-/*   Updated: 2025/05/02 19:47:13 by ibarbouc         ###   ########.fr       */
+/*   Updated: 2025/05/02 22:28:09 by ibarbouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,10 @@ char	*get_cmd(t_commande *command, char *s1)
 	i = 0;
 	if (!s1 || s1[0] == '\0')
 		return (perror("Command not found"), NULL);
-	if (s1[0] == '/')
-	{
-		if (access(s1, F_OK | X_OK) == 0)
-			return (ft_strdup(s1));
-		return (perror(s1), NULL);
-	}
+	if (s1[0] == '/' && access(s1, F_OK | X_OK) == 0)
+		return (ft_strdup(s1));
+	if (ft_strncmp(s1, "./", 2) == 0 && access(s1, F_OK | X_OK) == 0)
+		return (ft_strdup(s1));
 	str = get_path(command);
 	if (!str)
 		return (perror("PATH not found"), NULL);
