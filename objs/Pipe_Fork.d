806201bffb2342336153146308dc281d5e9ae0fe
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-05-05 19:32:29 +0200

    main final

--- objs/Pipe_Fork.d
+++
@@ -1,2 +0,0 @@
-objs/Pipe_Fork.o: srcs/Pipe_Fork.c includes/pipex.h \
-  lib/ft_printf/includes/ft_printf.h lib/libft/includes/libft.h
--- objs/main.d
+++
@@ -1,2 +0,0 @@
-objs/main.o: srcs/main.c lib/libft/includes/libft.h includes/pipex.h \
-  lib/ft_printf/includes/ft_printf.h
